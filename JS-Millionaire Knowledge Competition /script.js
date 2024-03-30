c0c391a1cb945e856e5320ab90abcb7af5775aaa
thyrdmc <mtyardimci45@gmail.com>
2024-03-30 22:43:23 +0300

    Completed - Class and Method Designs for knowledge competetion

---
+++ JS-Millionaire Knowledge Competition /script.js
@@ -0,0 +1,42 @@
+// ## Question Class 
+function Question(question, answer, correct_answer){
+    this.question = question;
+    this.answer = answer;
+    this.correct_answer = correct_answer;
+}
+
+// # Question Class Method
+Question.prototype.checkAnswer = function(input){
+    return input === this.correct_answer;
+}
+
+let questions = [
+    new Question("1-Question 1", {a: "A", b:"B", c:"C",}, "c"),
+    new Question("2-Question 2", {a: "A", b:"B", c:"C",}, "b"),
+    new Question("3-Question 3", {a: "A", b:"B", c:"C",}, "a"),
+];
+
+// ## Quiz Class 
+function Competetion(questions){
+    this.questions = questions;
+    this.questionNumber = 0;
+}
+
+// # Quiz Class Method
+Competetion.prototype.getQuestion = function(){
+    return this.questions[this.questionNumber];
+}
+
+const competetion = new Competetion(questions);
+
+
+document.querySelector(".btn-start").addEventListener("click", function(){
+    if(competetion.questions.length != competetion.questionNumber){
+        console.log(competetion.getQuestion());
+        competetion.questionNumber += 1;
+    }
+    else{
+        console.log("game over");
+    }
+    
+})
